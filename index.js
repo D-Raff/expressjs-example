46190a3708eafd5161ff92ee10e0a45d0a467023
D-Raff <damonraffels@gmail.com>
2024-02-09 16:16:20 +0200

    'updates'

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('welcome to express JS');
+import express from "express";
+
+//create an express app
+const app = express()
+const port = +process.env.PORT || 4000
+//create express router
+// patch is faster that put
+app.get('/', (req, res)=>{
+    res.json({
+        status: res.statusCode,
+        msg: "you are home"
+    })
+})
+
+app.listen(port)
+
+app.all('*', (req,res)=>{
+    res.json({
+        status: 404,
+        msg: '404 page'
+    })
+})
